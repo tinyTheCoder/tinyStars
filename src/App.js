4828744e8f9e17ef8b8c4964eec02d02b7ed961d
damini kumari <tinystars2023@gmail.com>
2023-07-12 14:23:03 +0530

    tracking event code on button implemented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,{Component} from 'react'
-import ReactGA from "react-ga4";
+import ReactGA from "react-ga";
 
 class App extends Component {
 
@@ -8,9 +8,21 @@ class App extends Component {
     ReactGA.initialize("G-R8THYHG60B");
   }
 
+  eventTrack = (category,action,label) =>{
+    console.log('ga eventL: category, action ,label',category,action,label)
+    ReactGA.event({
+      category:category,
+      action:action,
+      label:label
+    })
+  }
+
   render(){
      return (
-    <h1>Google Alaytics 4</h1>
+      <>
+          <h1>Google Alaytics 4</h1>
+          <button onClick={this.eventTrack("Main Page","Track Me Button","Button")} >TRACK ME</button>
+      </>
    
   )
   }
