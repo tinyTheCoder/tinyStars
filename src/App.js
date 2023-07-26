e7994f2e20083981b3af3142475fdec44d4827df
damini kumari <tinystars2023@gmail.com>
2023-07-26 16:48:54 +0530

    gtag addToCart code added for monitization

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{Component} from 'react'
 // import ReactGA from "react-ga";
 import ReactGA from "react-ga4";
 import { gtag, install } from 'ga-gtag';
-import toTrackMonitization from './analytics.js'
+// import toTrackMonitization from './analytics.js'
 
 class App extends Component {
 
@@ -12,19 +12,20 @@ class App extends Component {
     // ReactGA.initialize("UA-277395046-1");
   }
 
-  // eventTrack = () =>{
-  //   gtag("event","add_to_cart",{
-  //     currency:"USD",
-  //     value:7,
-  //     items:[
-  //       {
-  //         item_id : 109,
-  //         item_name:"my first product - babe",
-  //         price:106
-  //       }
-  //     ]
-  //   })
-  // }
+  ga4MonitizationTrack = () =>{
+    console.log('montization track for addToCart')
+    gtag("event","add_to_cart",{
+      currency:"USD",
+      value:7,
+      items:[
+        {
+          item_id : 109,
+          item_name:"my first product - babe",
+          price:106
+        }
+      ]
+    })
+  }
 
 
   // eventTrack = (category,action,label) =>{
@@ -46,7 +47,8 @@ class App extends Component {
       <>
           <h1>Google Alaytics 4</h1>
           <button onClick={()=>this.eventTrack("Main Page","Track Me Button Event","Button")} >TRACK ME</button>
-          <button onClick={()=>toTrackMonitization('addToCart')}>ADD TO CART</button>
+          {/* <button onClick={()=>toTrackMonitization('addToCart')}>ADD TO CART</button> */}
+          <button onClick={()=>this.ga4MonitizationTrack()}>ADD TO CART</button>
 
           <button onClick={()=>this.loginEvent()} >LOGIN ME</button>
       </>
