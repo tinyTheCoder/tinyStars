1fb5ac90d42038ce98b2a1452849f7850df4e319
damini kumari <tinystars2023@gmail.com>
2023-07-26 16:30:24 +0530

    gtag package added and one custom event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React,{Component} from 'react'
 // import ReactGA from "react-ga";
 import ReactGA from "react-ga4";
+import { gtag, install } from 'ga-gtag';
 import toTrackMonitization from './analytics.js'
 
 class App extends Component {
 
   componentDidMount(){
     ReactGA.initialize("G-KHQDLL6J28");
+    install('G-KHQDLL6J28'); 
     // ReactGA.initialize("UA-277395046-1");
   }
 
-  eventTrack = (category,action,label) =>{
-    console.log('ga eventL: category, action ,label',category,action,label)
-    ReactGA.event({
-      category:category,
-      action:action,
-      label:label
-    })
-  }
+  // eventTrack = () =>{
+  //   gtag("event","add_to_cart",{
+  //     currency:"USD",
+  //     value:7,
+  //     items:[
+  //       {
+  //         item_id : 109,
+  //         item_name:"my first product - babe",
+  //         price:106
+  //       }
+  //     ]
+  //   })
+  // }
+
+
+  // eventTrack = (category,action,label) =>{
+  //   console.log('ga eventL: category, action ,label',category,action,label)
+  //   ReactGA.event({
+  //     category:category,
+  //     action:action,
+  //     label:label
+  //   })
+  // }
 
   
+  loginEvent = () =>{
+    gtag('event', 'loginEvent', { 'method': 'Google' });
+  }
 
   render(){
      return (
@@ -27,6 +47,8 @@ class App extends Component {
           <h1>Google Alaytics 4</h1>
           <button onClick={()=>this.eventTrack("Main Page","Track Me Button Event","Button")} >TRACK ME</button>
           <button onClick={()=>toTrackMonitization('addToCart')}>ADD TO CART</button>
+
+          <button onClick={()=>this.loginEvent()} >LOGIN ME</button>
       </>
    
   )
